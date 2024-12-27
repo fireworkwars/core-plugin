4cf507cf23593a682b27bcc57ae6509b6f50d9f0
rolyPolyVole <yezhanting@gmail.com>
2024-12-27 17:20:55 +0000

    Improve logs

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/FireworkWarsCorePlugin.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/FireworkWarsCorePlugin.kt
@@ -15,21 +15,38 @@ class FireworkWarsCorePlugin : BasePlugin() {
 
     @Suppress("UnstableApiUsage")
     override fun onLoad() {
-        logger.info("=-=-=-=-=-=-=-=-=-=-=-=-= Firework Wars Lobby Plugin =-=-=-=-=-=-=-=-=-=-=-=-=-=")
-        logger.info("This is the start of Firework Wars Lobby Plugin logs.")
+        logger.info("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Firework Wars Core =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+        logger.info("This is the start of Firework Wars Core Plugin logs.")
         logger.info("Info: v" + pluginMeta.version + " by " + pluginMeta.website)
-        logger.info("=-=-=-=-=-=-=-=-=-=-=-=-= End of Plugin Info =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+        logger.info("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= End of Plugin Info =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+    }
+
+    override fun onEnable() {
+        logger.info("Starting core initialisation sequence...")
+        logger.info("Creating data folder...")
+
+        dataFolder.mkdir()
+        saveDefaultConfig()
+
+        logger.info("Successfully created data folder and loaded default configurations.")
+        logger.info("Loading player profiles...")
+
+        this.playerDataManager = PlayerDataManager(this)
 
         logger.info("Finished loading player profiles.")
         logger.info("Loaded ${playerDataManager.size} player profiles.")
 
+        logger.info("Initialising language manager...")
+
+        this.languageManager = LanguageManager(this)
+
         logger.info("Finished initialising language manager.")
         logger.info("Loaded ${languageManager.totalMessages} messages across ${languageManager.totalLanguages} languages.")
-    }
 
-    override fun onEnable() {
-        dataFolder.mkdir()
-        saveDefaultConfig()
+        logger.info("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Firework Wars Core =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+        logger.info("End of logs for Firework Wars Core Plugin.")
+        logger.info("Finished Firework Wars Core initialisation sequence.")
+        logger.info("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= All systems operational  =-=-=-=-=-=-=-=-=-=-=-=-=")
     }
 
     override fun onDisable() {
