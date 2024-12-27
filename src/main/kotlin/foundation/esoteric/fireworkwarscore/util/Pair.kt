994f971c4213fc0dc1419b40db11f7761f64704e
rolyPolyVole <yezhanting@gmail.com>
2024-12-27 21:59:53 +0000

    Make static of() method

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/util/Pair.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/util/Pair.kt
@@ -3,6 +3,7 @@ package foundation.esoteric.fireworkwarscore.util
 @Suppress("unused")
 class Pair<A, B>(val left: A, val right: B) {
     companion object {
+        @JvmStatic
         fun <A, B> of(a: A, b: B): Pair<A, B> {
             return Pair(a, b)
         }
