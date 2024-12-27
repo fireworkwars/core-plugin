2a9301f7dbbc6781ab25825a464a358a6036cf01
rolyPolyVole <yezhanting@gmail.com>
2024-12-27 17:21:10 +0000

    Make player data and language manager be lateinit and var

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/FireworkWarsCorePlugin.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/FireworkWarsCorePlugin.kt
@@ -7,8 +7,8 @@ import foundation.esoteric.fireworkwarscore.profiles.PlayerDataManager
 
 @Suppress("unused")
 class FireworkWarsCorePlugin : BasePlugin() {
-    override val playerDataManager: PlayerDataManager = PlayerDataManager(this)
-    override val languageManager: LanguageManager = LanguageManager(this)
+    override lateinit var playerDataManager: PlayerDataManager
+    override lateinit var languageManager: LanguageManager
 
     lateinit var fireworkWarsPluginData: FireworkWarsPluginData
     lateinit var lobbyPluginData: LobbyPluginData
