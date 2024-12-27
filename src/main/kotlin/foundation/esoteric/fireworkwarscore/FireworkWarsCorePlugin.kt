7b2b0e18b84d32be7f9ad119cfdc4f8998a837de
rolyPolyVole <yezhanting@gmail.com>
2024-12-27 16:09:57 +0000

    Extend `BasePlugin` and initialise player data and language manager

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/FireworkWarsCorePlugin.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/FireworkWarsCorePlugin.kt
@@ -1,10 +1,14 @@
 package foundation.esoteric.fireworkwarscore
 
-import foundation.esoteric.fireworkwarscore.intercom.FireworkWarsPluginData
-import foundation.esoteric.fireworkwarscore.intercom.LobbyPluginData
-import org.bukkit.plugin.java.JavaPlugin
+import foundation.esoteric.fireworkwarscore.communication.FireworkWarsPluginData
+import foundation.esoteric.fireworkwarscore.communication.LobbyPluginData
+import foundation.esoteric.fireworkwarscore.language.LanguageManager
+import foundation.esoteric.fireworkwarscore.profiles.PlayerDataManager
 
-class FireworkWarsCorePlugin : JavaPlugin() {
+class FireworkWarsCorePlugin : BasePlugin() {
+    override val playerDataManager: PlayerDataManager = PlayerDataManager(this)
+    override val languageManager: LanguageManager = LanguageManager(this)
+
     lateinit var fireworkWarsPluginData: FireworkWarsPluginData
     lateinit var lobbyPluginData: LobbyPluginData
 
