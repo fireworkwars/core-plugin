a1cce1e20810f644df471cdadf981c3269a00442
rolyPolyVole <yezhanting@gmail.com>
2024-12-29 23:31:28 +0100

    Add extension function `Player#sendMessage(Message, args)`

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/util/Extn.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/util/Extn.kt
@@ -1,9 +1,16 @@
 package foundation.esoteric.fireworkwarscore.util
 
+import foundation.esoteric.fireworkwarscore.FireworkWarsCorePlugin
+import foundation.esoteric.fireworkwarscore.language.Message
 import org.bukkit.Sound
 import org.bukkit.entity.Player
 
-//extend org.bukkit.player.Player and make a new playSound(Sound sound) method
+@Suppress("unused")
 fun Player.playSound(sound: Sound) {
     this.playSound(this, sound, 1.0F, 1.0F)
+}
+
+@Suppress("unused")
+fun Player.sendMessage(message: Message, vararg args: Any?) {
+    FireworkWarsCorePlugin.INSTANCE.languageManager.sendMessage(message, this, *args)
 }
