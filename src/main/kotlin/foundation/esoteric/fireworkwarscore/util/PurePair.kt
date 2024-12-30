5d8f6025ccc7f348d6a7395c7c663b4ec08219c6
rolyPolyVole <yezhanting@gmail.com>
2024-12-30 20:48:06 +0100

    Add more util methods to `PurePair`

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/util/PurePair.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/util/PurePair.kt
@@ -1,6 +1,9 @@
 package foundation.esoteric.fireworkwarscore.util
 
-@Suppress("unused")
+import java.util.Comparator.comparingDouble
+import java.util.Comparator.comparingInt
+
+@Suppress("unused", "MemberVisibilityCanBePrivate")
 class PurePair<T: Any>(left: T, right: T) : Pair<T, T>(left, right) {
     fun min(comparator: Comparator<T>): T {
         return if (comparator.compare(left, right) <= 0) left else right
@@ -8,5 +11,21 @@ class PurePair<T: Any>(left: T, right: T) : Pair<T, T>(left, right) {
 
     fun max(comparator: Comparator<T>): T {
         return if (comparator.compare(left, right) >= 0) left else right
+    }
+
+    fun min(intSupplier: (T) -> Int): T {
+        return min(comparingInt(intSupplier))
+    }
+
+    fun max(intSupplier: (T) -> Int): T {
+        return max(comparingInt(intSupplier))
+    }
+
+    fun min(doubleSupplier: (T) -> Double): T {
+        return min(comparingDouble(doubleSupplier))
+    }
+
+    fun max(doubleSupplier: (T) -> Double): T {
+        return max(comparingDouble(doubleSupplier))
     }
 }
