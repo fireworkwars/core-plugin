501901f5f3f69b565046d200872921d8d21a232c
rolyPolyVole <yezhanting@gmail.com>
2025-01-03 23:36:35 +0100

    Fix updating command sender's tablist instead of rank receiver

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/commands/operator/SetRankCommand.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/commands/operator/SetRankCommand.kt
@@ -59,7 +59,7 @@ class SetRankCommand(private val plugin: FireworkWarsCorePlugin) : CommandAPICom
             targetPlayer.sendMessage(Message.RANK_GRANTED, rank.toFormattedText())
         }
 
-        rank.updateTablist(player)
+        targetProfile.rank.updateTablist(targetPlayer)
         plugin.lobbyPluginData.updateScoreboards()
     }
 }
