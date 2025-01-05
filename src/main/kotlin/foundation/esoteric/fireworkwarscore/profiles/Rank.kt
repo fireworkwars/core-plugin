d1543a106150077216141f0106884db4f27cce17
rolyPolyVole <yezhanting@gmail.com>
2025-01-05 17:08:39 +0100

    Remove unneeded packet sending from `updateTablist()`

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/profiles/Rank.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/profiles/Rank.kt
@@ -1,17 +1,12 @@
 package foundation.esoteric.fireworkwarscore.profiles
 
-import foundation.esoteric.fireworkwarscore.util.NMSUtil
 import foundation.esoteric.fireworkwarscore.util.appendSpaceIfNotEmpty
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.empty
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket
-import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket.Action
-import net.minecraft.server.level.ServerPlayer
 import org.bukkit.entity.Player
-import org.bukkit.plugin.java.JavaPlugin
 
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 enum class Rank(val color: TextColor, private val prefixValue: String?, private val listOrder: Int) {
@@ -31,16 +26,9 @@ enum class Rank(val color: TextColor, private val prefixValue: String?, private 
         return prefix.appendSpaceIfNotEmpty().append(text(capitalised())).color(color).compact()
     }
 
-    fun updateTablist(player: Player, plugin: JavaPlugin) {
+    fun updateTablist(player: Player) {
         player.playerListName(formatPlayerName(player))
         player.playerListOrder = listOrder
-
-        val nmsPlayer = NMSUtil.toNMSEntity<ServerPlayer>(player)
-        val packet = ClientboundPlayerInfoUpdatePacket(Action.UPDATE_LIST_ORDER, nmsPlayer)
-
-        plugin.server.onlinePlayers.forEach {
-            NMSUtil.toNMSEntity<ServerPlayer>(it).connection.send(packet)
-        }
     }
 
     fun capitalised(): String {
