696d537290c7832634f19f6c119b613e8908b256
rolyPolyVole <yezhanting@gmail.com>
2025-01-15 19:02:14 +0000

    Revert `teleportPlayerAndNameTag()` addition to `LobbyPluginData`

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/communication/LobbyPluginData.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/communication/LobbyPluginData.kt
@@ -2,12 +2,10 @@ package foundation.esoteric.fireworkwarscore.communication
 
 import org.bukkit.Location
 import org.bukkit.World
-import org.bukkit.entity.Player
 
 @Suppress("unused")
 interface LobbyPluginData {
     fun getLobbySpawn(): Location
     fun isLobby(world: World): Boolean
     fun updateScoreboards()
-    fun teleportPlayerAndNameTag(player: Player, location: Location)
 }
