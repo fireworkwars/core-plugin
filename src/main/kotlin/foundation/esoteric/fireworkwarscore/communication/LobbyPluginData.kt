7e138a49d7a44dc070939696f328f629f3c834ae
rolyPolyVole <yezhanting@gmail.com>
2025-01-15 17:45:28 +0000

    Add `teleportPlayerAndNameTag()` to lobby data holder

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/communication/LobbyPluginData.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/communication/LobbyPluginData.kt
@@ -2,10 +2,12 @@ package foundation.esoteric.fireworkwarscore.communication
 
 import org.bukkit.Location
 import org.bukkit.World
+import org.bukkit.entity.Player
 
 @Suppress("unused")
 interface LobbyPluginData {
     fun getLobbySpawn(): Location
     fun isLobby(world: World): Boolean
     fun updateScoreboards()
+    fun teleportPlayerAndNameTag(player: Player, location: Location)
 }
