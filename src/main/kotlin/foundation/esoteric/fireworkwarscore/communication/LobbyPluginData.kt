edecad37f0e8e3bcb4c4a7f1d4a8b2d935c858e9
rolyPolyVole <yezhanting@gmail.com>
2025-01-18 12:44:36 +0000

    Add `setTagVisibility()` method

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/communication/LobbyPluginData.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/communication/LobbyPluginData.kt
@@ -2,10 +2,12 @@ package foundation.esoteric.fireworkwarscore.communication
 
 import org.bukkit.Location
 import org.bukkit.World
+import org.bukkit.entity.Player
 
 @Suppress("unused")
 interface LobbyPluginData {
     fun getLobbySpawn(): Location
     fun isLobby(world: World): Boolean
     fun updateScoreboards()
+    fun setTagVisibility(player: Player, visible: Boolean)
 }
