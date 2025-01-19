01da56bfa079eeea02deb5d1aa6249a457de69b6
rolyPolyVole <yezhanting@gmail.com>
2025-01-19 15:05:53 +0000

    Add chat channel manager

---
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/managers/ChatChannelManager.kt
@@ -0,0 +1,40 @@
+package foundation.esoteric.fireworkwarscore.managers
+
+import foundation.esoteric.fireworkwarscore.FireworkWarsCorePlugin
+import foundation.esoteric.fireworkwarscore.interfaces.Event
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import java.util.*
+
+class ChatChannelManager(private val plugin: FireworkWarsCorePlugin) : Event {
+    private val playerChannels = mutableMapOf<UUID, ChatChannel>()
+
+    override fun register() {
+        plugin.registerEvent(this)
+    }
+
+    fun getChannel(player: UUID): ChatChannel {
+        return playerChannels[player] ?: ChatChannel.ALL
+    }
+
+    fun setChannel(player: UUID, channel: ChatChannel) {
+        playerChannels[player] = channel
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        playerChannels[event.player.uniqueId] = ChatChannel.ALL
+    }
+
+    @EventHandler
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        playerChannels.remove(event.player.uniqueId)
+    }
+
+    enum class ChatChannel {
+        ALL,
+        FRIEND,
+        PARTY
+    }
+}
