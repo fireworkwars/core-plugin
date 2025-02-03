ec1cdccc82d249cb82be0d6e343d5cd81627ca08
rolyPolyVole <yezhanting@gmail.com>
2025-02-03 18:15:20 +0000

    Remove map-related config option

--- src/main/kotlin/foundation/esoteric/fireworkwarscore/config/PluginConfig.kt
+++ src/main/kotlin/foundation/esoteric/fireworkwarscore/config/PluginConfig.kt
@@ -4,7 +4,6 @@ import foundation.esoteric.fireworkwarscore.FireworkWarsCorePlugin
 
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 class PluginConfig(plugin: FireworkWarsCorePlugin) {
-    val updateMaps: Boolean = plugin.config.getBoolean("update-maps-on-start")
     val defaultLanguage: String = plugin.config.getString("default-language")!!
     val serverIp: String = plugin.config.getString("server-ip")!!
     val discordInvite: String = plugin.config.getString("discord-invite")!!
