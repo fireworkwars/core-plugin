12d11f090f6282111009bf455cb5f6fc5be584c7
rolyPolyVole <yezhanting@gmail.com>
2025-02-23 21:11:02 +0100

    Prevent duplicate ids being added to the expiry tasks map

--- src/main/kotlin/xyz/fireworkwars/core/util/ExpiryManager.kt
+++ src/main/kotlin/xyz/fireworkwars/core/util/ExpiryManager.kt
@@ -1,16 +1,20 @@
 package xyz.fireworkwars.core.util
 
+import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitTask
-import xyz.fireworkwars.core.FireworkWarsCorePlugin
 
 class ExpiryManager(private val plugin: JavaPlugin, private val expiryDuration: Long) {
     private val expiryTasks: MutableMap<String, BukkitTask> = mutableMapOf()
 
     fun addExpiryTask(id: String, onExpire: Runnable) {
-        val task = plugin.runTaskLater(expiryDuration) {
+        if (expiryTasks.containsKey(id)) {
+            throw IllegalArgumentException("Expiry task with id $id already exists")
+        }
+
+        val task = plugin.server.scheduler.runTaskLater(plugin, Runnable {
             onExpire.run()
             expiryTasks.remove(id)
-        }
+        }, expiryDuration)
 
         expiryTasks[id] = task
     }
