a42c54697eb1879dd0fe9e4d3c5dc7f8e3d39803
rolyPolyVole <yezhanting@gmail.com>
2025-03-06 23:08:34 +0000

    Update version and use military alphabet for some reason

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val buildToMain = false
 val buildToLobby = false
 
 group = "xyz.fireworkwars"
-version = "1.14.1"
+version = "1.14.1-bravo"
 description = "Required dependencies & core functionality for a Firework Wars server."
 
 val pluginName = "FireworkWarsCore"
