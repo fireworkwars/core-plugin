14b54422135fe55f3518789218abb6041ac93f6a
rolyPolyVole <yezhanting@gmail.com>
2025-03-18 23:06:09 +0000

    Add more methods for lobby fireworks

--- src/main/kotlin/xyz/fireworkwars/core/util/FireworkCreator.kt
+++ src/main/kotlin/xyz/fireworkwars/core/util/FireworkCreator.kt
@@ -13,14 +13,14 @@ class FireworkCreator {
     companion object {
         @JvmStatic
         @JvmOverloads
-        fun createFirework(color: Color, amount: Int, type: FireworkEffect.Type = FireworkEffect.Type.BALL, flicker: Boolean = false, trail: Boolean = false): ItemStack {
+        fun createColoredFirework(color: Color, fade: Color, amount: Int, type: FireworkEffect.Type = FireworkEffect.Type.BALL, flicker: Boolean = false, trail: Boolean = false): ItemStack {
             val item = ItemStack(org.bukkit.Material.FIREWORK_ROCKET)
             val meta = item.itemMeta as FireworkMeta
 
             for (i in (1..amount)) {
                 meta.addEffect(FireworkEffect.builder()
-                    .withColor(Color.WHITE)
-                    .withFade(color)
+                    .withColor(color)
+                    .withFade(fade)
                     .with(type)
                     .flicker(flicker)
                     .trail(trail)
@@ -29,6 +29,13 @@ class FireworkCreator {
 
             item.itemMeta = meta
             return item
+
+        }
+
+        @JvmStatic
+        @JvmOverloads
+        fun createFirework(color: Color, amount: Int, type: FireworkEffect.Type = FireworkEffect.Type.BALL, flicker: Boolean = false, trail: Boolean = false): ItemStack {
+            return createColoredFirework(Color.WHITE, color, amount, type, flicker, trail)
         }
 
         @JvmStatic
@@ -42,9 +49,31 @@ class FireworkCreator {
         }
 
         @JvmStatic
+        fun randomLobbyFirework(): ItemStack {
+            return this.createColoredFirework(
+                listOf(Color.BLUE, Color.PURPLE, Color.AQUA, Color.RED, Color.ORANGE, Color.LIME, Color.GREEN).random(),
+                listOf(Color.BLUE, Color.PURPLE, Color.AQUA, Color.RED, Color.WHITE).random(),
+                (1..4).random(),
+                listOf(FireworkEffect.Type.BURST, FireworkEffect.Type.STAR).random(),
+                Random.nextBoolean(),
+                Random.nextBoolean())
+        }
+
+        @JvmStatic
         fun sendSupplyDropFirework(location: Location, flightTicks: Int) {
             val supplyDropFirework = this.randomSupplyDropFirework()
             val meta = supplyDropFirework.itemMeta as FireworkMeta
+
+            location.world.spawn(location, Firework::class.java).apply {
+                this.fireworkMeta = meta
+                this.ticksToDetonate = flightTicks
+            }
+        }
+
+        @JvmStatic
+        fun sendLobbyFirework(location: Location, flightTicks: Int) {
+            val lobbyFirework = this.randomLobbyFirework()
+            val meta = lobbyFirework.itemMeta as FireworkMeta
 
             location.world.spawn(location, Firework::class.java).apply {
                 this.fireworkMeta = meta
