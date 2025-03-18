2c712ca5507437961d0697099ca977a2dc72130a
rolyPolyVole <yezhanting@gmail.com>
2025-03-18 22:03:48 +0000

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val buildToMain = false
 val buildToLobby = false
 
 group = "xyz.fireworkwars"
-version = "1.15.1"
+version = "1.16.0"
 description = "Required dependencies & core functionality for a Firework Wars server."
 
 val pluginName = "FireworkWarsCore"
