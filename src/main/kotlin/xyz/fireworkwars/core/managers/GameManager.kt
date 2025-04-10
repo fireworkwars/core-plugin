ddab5d7b877ba146a4924b54a7458c952045cfc0
rolyPolyVole <yezhanting@gmail.com>
2025-04-10 23:01:47 +0200

    Add a game manager

---
+++ src/main/kotlin/xyz/fireworkwars/core/managers/GameManager.kt
@@ -0,0 +1,20 @@
+package xyz.fireworkwars.core.managers
+
+import org.bukkit.entity.Player
+import xyz.fireworkwars.core.FireworkWarsCorePlugin
+import xyz.fireworkwars.core.interfaces.GamePlayer
+import java.util.function.Function
+
+class GameManager(private val plugin: FireworkWarsCorePlugin) {
+    private val gamePlayerSources = mutableListOf<Function<Player, GamePlayer?>>()
+
+    fun addGamePlayerSource(source: Function<Player, GamePlayer?>) {
+        gamePlayerSources.add(source)
+    }
+
+    fun getGamePlayer(player: Player): GamePlayer? {
+        return gamePlayerSources
+            .mapNotNull { it.apply(player) }
+            .getOrNull(0)
+    }
+}
